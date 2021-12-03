35c283bf9044ba69e4831ff8d73b40176c5b0e34
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2021-12-03 16:18:39 -0600

    Completed Comparer Class, Sorted Flavors Alphabetically

---
+++ IceCreamShop/FlavorComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IceCreamShop
+{
+    public class FlavorComparer : IComparer<Flavor>
+    {
+        public int Compare(Flavor flavor1, Flavor flavor2)
+        {
+            // this Compare method takes in 2 Flavor objects
+            // and compares the names alphabetically
+            return string.Compare(flavor1.Name, flavor2.Name);
+        }
+    }
+}
--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -9,15 +9,58 @@ namespace IceCreamShop
         {
             Case menu = new Case();
             List<Flavor> availableFlavors = menu.Flavors;
+            Console.WriteLine("Before:");
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.Write($"{flavor.Name}, ");
+            }
+
+            FlavorComparer comparer = new FlavorComparer();
+
+            availableFlavors.Sort(comparer);
+
+            Console.WriteLine();
+
+            Console.WriteLine("After:");
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.Write($"{flavor.Name}, ");
+            }
+
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+
+
+
+
+            // Lets test out this Sort() method in C#
+           /* List<string> stringSortingTest = new List<string>();
+            stringSortingTest.Add("Pearce");
+            stringSortingTest.Add("Susan");
+            stringSortingTest.Add("John");
+            stringSortingTest.Add("Todd");
+            foreach (string str in stringSortingTest)
+            {
+                Console.Write($"{str}, ");
+            }
+            Console.WriteLine();
+
+            stringSortingTest.Sort();
+            foreach (string str in stringSortingTest)
+            {
+                Console.Write($"{str}, ");
+            }
+
+            Console.WriteLine();
+*/
         }
     }
 }
